031455c4c82952d1eac8be669dc4b0c3b08b2ef8
pauliussx <skucas.paulius@gmail.com>
2016-12-10 19:53:33 +0200

    hostname

--- resources/config/db.php
+++ resources/config/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$config['db']['hostname'] = '';
+$config['db']['hostname'] = 'db.if.ktu.lt';
 $config['db']['username'] = '';
 $config['db']['password'] = '';
 $config['db']['database'] = '';
