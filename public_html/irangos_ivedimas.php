f86294da072bbd508d5af9aeae13f9a1d7247fcd
pauliussx <skucas.paulius@gmail.com>
2016-12-11 19:23:20 +0200

    dadeti radio button irangos ivedime

--- public_html/irangos_ivedimas.php
+++ public_html/irangos_ivedimas.php
@@ -1,13 +1,25 @@
 <?php
     require_once '../app.php';
     
-    $pageTitle = 'Įrangos peržiūra';
+    $pageTitle = 'Pridėti įrangą';
     
     require_once TEMPLATES_PATH . 'menu.php';
     
 ?>
 
-<h3>vidus</h3>
+<div class="container">
+    <div class="row">
+	<h3>Pridėti įrangą</h3>
+    </div>
+    <div class="row">
+        <div class="radio">
+            <label><input type="radio" name="optradio" value ="Navigacija">Kompiuteris</label>
+	</div>
+	<div class="radio">
+            <label><input type="radio" name="optradio" value ="Kėdutė">Monitorius </label>
+	</div>
+    </div>
+</div>
 
 
 <?php
