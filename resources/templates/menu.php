71a1bfd533b35a4882cade6bf7abfbefe6af03a3
pauliussx <skucas.paulius@gmail.com>
2016-12-11 16:23:24 +0200

    priedai pervadinti i papildoma iranga

--- resources/templates/menu.php
+++ resources/templates/menu.php
@@ -19,7 +19,7 @@
                     </div>
                     <ul class="nav navbar-nav">
                         <li><a href="#">Automobiliai</a></li>  
-                        <li><a href="#">Priedai</a></li>
+                        <li><a href="#">Papildoma įranga</a></li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <li><a href="#"><span class="glyphicon glyphicon-user"></span> Registracija</a></li>
