3b86c1efee6062c3d70f3ef843a91565ce0b9d28
pauliussx <skucas.paulius@gmail.com>
2016-12-11 16:47:59 +0200

    Formuojama įrangos peržiūros lentelė

--- public_html/irangos_perziura.php
+++ public_html/irangos_perziura.php
@@ -5,9 +5,49 @@
     
     require_once TEMPLATES_PATH . 'menu.php';
     
+    $dbc = mysqli_connect($config['db']['hostname'], $config['db']['username'], 
+                                $config['db']['password'], $config['db']['database']);
+		if (!$dbc) 
+		{
+			die('Negaliu prisijungti: ' . mysqli_error($dbc));
+		}
 ?>
 
-<h3>vidus</h3>
+<div class="container">
+    <div class="row">
+	<h3>Navigacijos</h3>
+    </div>
+    <table class="table table-bordered">
+	<thead>
+            <tr>
+		<th>Pavadinimas</th>
+		<th>Žemėlapių metai</th>
+		<th>Įstrižainė</th>  
+		<th>Atmintis</th>
+		<th>Bluetooth</th>
+		<th>Kaina</th>
+		<th>Komentarai</th>
+                <th>Įvedimo data</th>
+            </tr>
+	</thead>
+	<tbody>
+            <!--<?php
+		while ($row = mysqli_fetch_array($result))
+                {
+                    echo "<tr>"
+                            .	"<td>" . $row['komp_m'] . "</td>"
+                            .	"<td>" . $row['mon_m'] . "</td>"
+                            .	"<td>" . $row['klav_m'] . "</td>"
+                            .	"<td>" . $row['pel_m'] . "</td>"
+                            .	"<td>" . array_sum(array($row['komp_k'], $row['mon_k'], $row['klav_k'], $row['pel_k'])) . "</td>"
+                            .	"<td>" . $row['name'] . "</td>"
+                            .	"<td>" . $row['mail'] . "</td>"
+                            .	"</tr>";
+		}
+            ?>-->
+	</tbody>
+    </table>
+</div>
 
 
 <?php
