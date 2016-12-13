375edec971abc68f30f32b67715d3d44dd0d7b73
pauliussx <skucas.paulius@gmail.com>
2016-12-13 19:38:09 +0200

    uzsakymuose pridetas meniu

--- public_html/uzsakymu_perziura.php
+++ public_html/uzsakymu_perziura.php
@@ -1,8 +1,7 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    require_once '../app.php';
+    
+    $pageTitle = 'Užsakymai';
+    
+    require_once TEMPLATES_PATH . 'menu.php';
+?>
