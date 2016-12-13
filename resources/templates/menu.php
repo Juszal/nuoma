88f8b5308df9261f18b106c55a76600bd29ba6f3
pauliussx <skucas.paulius@gmail.com>
2016-12-13 18:22:50 +0200

    i menu dadetas pasiekiamumas i datettimepicker

--- resources/templates/menu.php
+++ resources/templates/menu.php
@@ -4,9 +4,12 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=9; text/html; charset=utf-8"/> 
         <title><?php echo $pageTitle ?></title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo $config['baseUrl'] . 'css/bootstrap-datetimepicker.min.css'?>">
         <link href="include/styles.css" rel="stylesheet" type="text/css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="<?php echo $config['baseUrl'] . 'js/moment.js'?>"></script>
+        <script src="<?php echo $config['baseUrl'] . 'js/bootstrap-datetimepicker.js'?>"></script>
 	</head>
         <body>
             <nav class="navbar navbar-inverse">
